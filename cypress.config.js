620313afedde7b946528c1298ea2272df90f1fc6
“Serhii <s.maksyuta1@gmail.com>
2023-05-14 22:59:47 +0300

    homework is done

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  pageLoadTimeout: 25000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
+
+
